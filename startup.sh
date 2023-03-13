23f8c2ec48e32b2f3f86a489b5dd59214493c5d4
amac <freeamac@gmail.com>
2023-03-12 18:09:07 -0700

    Change to run the run module

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
-    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot run:app
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 run:run
