4e0d5f914e3904cd675eb5adc7255242d42b6c67
amac <5177663+freeamac@users.noreply.github.com>
2023-09-04 08:30:11 -0700

    Add objects to compose a list of LPs
    The objects created:
      + Singleton list of Artist objects
      + Artist object
      + Song object
      + TrackList object
      + Singleton list of LP objects
      + LP object
    
      The Artist and LP objects are created
    from a factory function in their respective singleton list objects
    
    Tests included

---
+++ app/lps/lps_objects.py
@@ -0,0 +1,352 @@
+"""
+Definition of all the objects required for a list of LPs:
+    + A set of LPs is a singleton holding a set of all LP objects
+    + An LP object has a title string, Artist object, list of Track objects
+      and an optional mixer Artist object
+    + An Artist object has a name string, a set of LP objects
+    + A set of Artists is a singleton holding a set of all Artist objects
+    + A Track object has an option side string and a list of Song objects
+    + A Song object has a title string, an option main Artist object,
+      an optional list of Additional Artist objects and an option mix
+      string
+    + a Additional Artist object which is essential a tuple of
+      a prequel string, artist object and sequel string which
+      is used to compose appropriate formatting where songs are
+      done in a collaboration
+
+LP objects can be deleted from the set of LPs which will also remove that
+lp from the set of LP objects referenced by the lp artist.
+
+It is possible for an Artist object to not reference any lps either due to
+there last lp being deleted or they are only associated with a song(s) on
+an lp.
+"""
+
+from typing import Optional
+
+
+class LPException(Exception):
+    pass
+
+
+class ArtistException(Exception):
+    pass
+
+
+class SongException(Exception):
+    pass
+
+
+class TrackListException(Exception):
+    pass
+
+
+class _Artist():
+
+    @property
+    def lps(self) -> set:
+        return self._lps
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def __init__(self, name) -> None:
+        self._name = name
+        self._lps = set()
+
+    def add_lp(self, lp) -> None:
+        if type(lp) is not _LP:
+            raise LPException('{} is not an LP object'.format(lp))
+        if lp in self._lps:
+            raise LPException('{} already in list of LPs for {}'.format(lp.name, self.name))
+        else:
+            self._lps.add(lp)
+
+    def delete_lp(self, lp) -> None:
+        if lp not in self._lps:
+            raise LPException('{} not in list of LPs for {}'.format(lp.title, self.name))
+        else:
+            self._lps.remove(lp)
+
+    def update_name(self, new_name) -> None:
+        self._name = new_name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
+
+
+class Artists():
+    _instance = None
+    _artists = set()
+
+    @property
+    def artists(self) -> list:
+        return self._artists
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Artists, cls).__new__(cls)
+        return cls._instance
+
+    def create_Artist(self, name, skip_adding_to_artists_list=False) -> _Artist:
+        if name is None:
+            raise ArtistException('An artist must have a name')
+        result = self.find_artist(name)
+        if result is not None:
+            return result
+        new_artist = _Artist(name)
+        if not skip_adding_to_artists_list:
+            self.add_artist(new_artist)
+        return new_artist
+
+    def add_artist(self, artist) -> None:
+        if type(artist) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(artist))
+        if artist in self.artists or self.find_artist(artist.name) is not None:
+            raise ArtistException('Artist {} already exists'.format(artist))
+        self._artists.add(artist)
+
+    def delete_artist(self, artist) -> None:
+        if artist not in self.artists:
+            raise ArtistException('Artist {} does not exist'.format(artist))
+        else:
+            self._artists.remove(artist)
+
+    def artist_exists(self, artist) -> bool:
+        return artist in self.artists
+
+    def find_artist(self, artist_name) -> Optional[_Artist]:
+        for artist in self._artists:
+            if artist.name == artist_name:
+                return artist
+
+    def __str__(self) -> str:
+        string = ''
+        for artist in self.artists:
+            string += '{}\n'.format(artist)
+        return string
+
+
+class Additional_Artist():
+
+    def __init__(self, artist, prequel=None, sequel=None) -> None:
+        if type(artist) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(artist))
+        self.artist = artist
+        self.prequel = prequel
+        self.sequel = sequel
+
+    def __str__(self) -> str:
+        if self.prequel is not None:
+            string = '{}{}'.format(self.prequel, self.artist)
+        else:
+            string = '{}'.format(self.artist)
+        if self.sequel is not None:
+            string += self.sequel
+        return string
+
+
+class Song():
+
+    def __init__(self, title, main_artist=None, additional_artists=[], mix=None) -> None:
+        self.title = title
+        if main_artist is not None and type(main_artist) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(main_artist))
+        self.main_artist = main_artist
+
+        # TODO: ensure a the passed list contains only additional_artists elements
+        self.additional_artists = additional_artists
+        self.mix = mix
+
+        # TODO: Add artists to artist list if the do not already exist
+
+    def __str__(self) -> str:
+        string = '{}\n'.format(self.title)
+        if self.main_artist is not None:
+            string += '{}\n'.format(self.main_artist)
+        if self.additional_artists != []:
+            for additional_artist in self.additional_artists:
+                string += '{}'.format(additional_artist)
+            string += '\n'
+        if self.mix is not None:
+            string += '({})\n'.format(self.mix)
+        return string
+
+
+class TrackList():
+
+    @property
+    def side(self) -> Optional[str]:
+        return self.side
+
+    @property
+    def side_mixer(self) -> Optional[str]:
+        return self.side_mixer
+
+    @ property
+    def song_list(self) -> list:
+        return self._song_list
+
+    def __init__(self, side=None, side_mixer=None) -> None:
+        self._side = side
+        if side_mixer is not None and type(side_mixer) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(side_mixer))
+        self._side_mixer = side_mixer
+        self._song_list = []
+
+    def add_song(self, song) -> None:
+        """ Songs are appended to the end of the song list and thus an ordered list """
+        if type(song) is Song:
+            self._song_list.append(song)
+        else:
+            raise SongException('{} is not a song'.format(song))
+
+    def remove_song(self, song) -> None:
+        if song in self.song_list:
+            self._song_list.remove(song)
+
+    def has_song(self, song) -> bool:
+        return song in self.song_list
+
+    def get_song_from_title(self, song_title) -> Optional[Song]:
+        for song in self.song_list:
+            if song.title == song_title:
+                return song
+
+    def __str__(self) -> str:
+        if self._side is not None:
+            string = '{}'.format(self._side)
+        else:
+            string = ''
+        if self._side_mixer is not None:
+            string += 'mixed by {}'.format(self._side_mixer)
+        string += '\n'
+        for counter, song in enumerate(self._song_list):
+            string += '{:2d}. {}'.format(counter + 1, song)
+        return string
+
+
+class _LP():
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def artist(self) -> _Artist:
+        return self._artist
+
+    @property
+    def date(self) -> int:
+        return self._date
+
+    @property
+    def mixer(self) -> Optional[_Artist]:
+        return self._mixer
+
+    @property
+    def tracks(self) -> list:
+        return self._tracks
+
+    def __init__(self, title, artist, date, mixer) -> None:
+        self._title = title
+        if type(artist) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(artist))
+        self._artist = artist
+        if type(date) is not int:
+            raise TypeError('Date must be an int value')
+        self._date = date
+        if mixer is not None and type(mixer) is not _Artist:
+            raise ArtistException('{} is not an Artist object'.format(mixer))
+        self._mixer = mixer
+
+        # Tracks must be set at this level or a reference will exist in the
+        # singleton and keep being appended to through "add_track()". Ensure
+        # starts empty with and instance level variable which can only be
+        # updated through an instance level call to "add_track[]".
+        self._tracks = []
+
+    def add_track(self, track) -> None:
+        if type(track) is TrackList:
+            self._tracks.append(track)
+        else:
+            raise TrackListException('{} is not a track list'.format(track))
+
+    def has_song(self, song) -> bool:
+        for track in self._tracks:
+            if track.has_song(song):
+                return True
+        return False
+
+    def get_song_from_title(self, song_title) -> Optional[Song]:
+        for track in self._tracks:
+            result = track.get_song_from_title(song_title)
+            if result is not None:
+                return result
+
+    def __str__(self) -> str:
+        string = '{}\n{}\n'.format(self.title, self.artist)
+        if self.mixer is not None:
+            string += 'Mixed by {}\n'.format(self.mixer)
+        string += '{}'.format(self.date)
+        for track in self.tracks:
+            string += str(track)
+        return string
+
+
+class LPs():
+    _instance = None
+    _lps = list()
+
+    @property
+    def lps(self) -> list:
+        return self._lps
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(LPs, cls).__new__(cls)
+        return cls._instance
+
+    def create_LP(self, title, artist, date, mixer=None, skip_adding_to_lp_list=False) -> _LP:
+        result = self.find_lp_by_title(title)
+        if result is not None:
+            return result
+        new_lp = _LP(title, artist, date, mixer)
+        artist.add_lp(new_lp)
+        if mixer is not None:
+            mixer.add_lp(new_lp)
+        if not skip_adding_to_lp_list:
+            self.add_lp(new_lp)
+        return new_lp
+
+    def add_lp(self, lp) -> None:
+        if type(lp) is not _LP:
+            raise LPException('{} is not an LP object'.format(lp))
+        if lp in self._lps:
+            raise LPException('LP {} already exists'.format(lp))
+        self._lps.append(lp)
+
+    def delete_lp(self, lp) -> None:
+        if lp in self.lps:
+            lp.artist.delete_lp(lp)
+            if lp.mixer is not None:
+                lp.mixer.delete_lp(lp)
+            self._lps.remove(lp)
+
+    def lp_exists(self, lp) -> bool:
+        return lp in self.lps
+
+    def find_lp_by_title(self, title) -> Optional[_LP]:
+        for lp in self.lps:
+            if lp.title == title:
+                return lp
+
+    def __str__(self) -> str:
+        string = ''
+        for lp in self.lps:
+            string += str(lp)
+        return string
---
+++ tests/test_lps.py
@@ -0,0 +1,257 @@
+import unittest
+
+import pytest
+
+from app.lps.lps_objects import (
+    Additional_Artist,
+    ArtistException,
+    Artists,
+    LPs,
+    LPException,
+    Song,
+    SongException,
+    TrackList,
+    TrackListException
+)
+
+
+class LPTestCase(unittest.TestCase):
+
+    def test_Artist(self):
+
+        artist = Artists().create_Artist('VArious Artists')
+        self.assertEqual('VArious Artists', artist.name)
+        self.assertEqual(artist.lps, set())
+        artist.update_name('Various Artists')
+        self.assertNotEqual('VArious Artists', artist.name)
+        self.assertEqual('Various Artists', artist.name)
+        self.assertEqual(artist.lps, set())
+        self.assertEqual('Various Artists', str(artist))
+
+    def test_Artists(self):
+        artist_1 = Artists().create_Artist('VArious Artists')
+        artist_2 = Artists().create_Artist('Disco D')
+        artist_3 = Artists().create_Artist('Joe Cocker', skip_adding_to_artists_list=True)
+        artists_1 = Artists()
+        artists_2 = Artists()
+
+        # Test singleton
+        self.assertIs(artists_1, artists_2)
+
+        # Test existence of artists
+        self.assertTrue(artists_1.artist_exists(artist_1))
+        self.assertTrue(artists_2.artist_exists(artist_1))
+        self.assertTrue(artists_2.artist_exists(artist_2))
+        self.assertFalse(artists_1.artist_exists(artist_3))
+
+        # Test we can only add valid artists
+        with pytest.raises(ArtistException):
+            artists_1.add_artist('Hello')
+
+        # Change artist name and find it
+        artist_1.update_name('Various Artists')
+        self.assertTrue(artists_1.artist_exists(artist_1))
+
+        # Test removing an artist
+        artists_1.add_artist(artist_3)
+        self.assertTrue(artists_1.artist_exists(artist_3))
+        artists_1.delete_artist(artist_3)
+        self.assertFalse(artists_1.artist_exists(artist_3))
+
+        # Test finding artists by name
+        self.assertTrue(artists_1.find_artist('Disco D'))
+        self.assertTrue(artists_1.find_artist('Various Artists'))
+        self.assertFalse(artists_1.find_artist('VArious Artists'))
+
+        # Check string representation. Need to split into list as
+        # set order is mutable over tests.
+        self.assertSetEqual(set(['Disco D', 'Various Artists', '']), set(str(artists_2).split('\n')))
+
+    def test_additional_artist(self):
+        artist = Artists().create_Artist('Disco D')
+        additional_artist = Additional_Artist(artist, prequel=' and ')
+        self.assertEqual(' and Disco D', str(additional_artist))
+        additional_artist = Additional_Artist(artist, sequel=' (mixer)')
+        self.assertEqual('Disco D (mixer)', str(additional_artist))
+        additional_artist = Additional_Artist(artist, prequel=' vs ', sequel=' (mixer)')
+        self.assertEqual(' vs Disco D (mixer)', str(additional_artist))
+
+        # Test can only add valid artists
+        with pytest.raises(ArtistException):
+            additional_artist = Additional_Artist('Hello')
+
+    def test_song(self):
+        song = Song('Wait A Minute')
+        self.assertEqual('Wait A Minute\n', str(song))
+        artist = Artists().create_Artist('DJ Nasty')
+        song = Song('Wait A Minute', main_artist=artist)
+        self.assertEqual('Wait A Minute\nDJ Nasty\n', str(song))
+        song = Song('Wait A Minute', main_artist=artist, mix='Red Box remix')
+        self.assertEqual('Wait A Minute\nDJ Nasty\n(Red Box remix)\n', str(song))
+        additional_artist_1 = Additional_Artist(Artists().create_Artist('DJ Nasty'))
+        additional_artist_2 = Additional_Artist(Artists().create_Artist('Disco D'), prequel=' vs ', sequel=' (mixer)')
+        song = Song('Wait A Minute', additional_artists=[additional_artist_1, additional_artist_2], mix='Red Box remix')
+        self.assertEqual('Wait A Minute\nDJ Nasty vs Disco D (mixer)\n(Red Box remix)\n', str(song))
+
+    def test_tracklist(self):
+        song_1 = Song('Jump To The Beat', Artists().create_Artist('Dannii Minogue'), mix='12" Mix')
+        song_2 = Song('Jump!', Artists().create_Artist('The Movement'), mix='Everybody Mix')
+        song_3 = Song('Pull Our Love Together', Artists().create_Artist('Pandella'), mix='Komix Club Mix')
+        track = TrackList(side='Side 1')
+
+        # Test song addition
+        track.add_song(song_1)
+        track.add_song(song_2)
+        self.assertTrue(track.has_song(song_1))
+        self.assertFalse(track.has_song(song_3))
+
+        # Test cannot add invalid song
+        with pytest.raises(SongException):
+            track.add_song('Hello')
+
+        # Test song removal
+        track.add_song(song_3)
+        track.remove_song(song_1)
+        track.remove_song(song_3)
+        self.assertTrue(track.has_song(song_2))
+        self.assertFalse(track.has_song(song_1))
+        self.assertFalse(track.has_song(song_3))
+        track.remove_song(song_2)
+        self.assertFalse(track.has_song(song_2))
+
+        # Test song title searching
+        track.add_song(song_1)
+        track.add_song(song_2)
+        track.add_song(song_3)
+        self.assertEqual(track.get_song_from_title('Jump!'), song_2)
+        self.assertIsNone(track.get_song_from_title('Jump'))
+
+        # Test string output
+        expected_string = 'Side 1\n'
+        expected_string += ' 1. Jump To The Beat\nDannii Minogue\n(12" Mix)\n'
+        expected_string += ' 2. Jump!\nThe Movement\n(Everybody Mix)\n'
+        expected_string += ' 3. Pull Our Love Together\nPandella\n(Komix Club Mix)\n'
+        self.assertEqual(expected_string, str(track))
+
+    def test_lp(self):
+        song_1 = Song('Jump To The Beat', Artists().create_Artist('Dannii Minogue'), mix='12" Mix')
+        song_2 = Song('Jump!', Artists().create_Artist('The Movement'), mix='Everybody Mix')
+        song_3 = Song('Pull Our Love Together', Artists().create_Artist('Pandella'), mix='Komix Club Mix')
+        song_4 = Song('Latino Mambo', Artists().create_Artist('Latin Side Od Soul'), mix='Dance Mix')
+        track = TrackList(side='Side 1')
+        track.add_song(song_1)
+        track.add_song(song_2)
+        track.add_song(song_3)
+
+        album_artist = Artists().create_Artist('Various Artists')
+        album_mixer = Artists().create_Artist('DJ Funk')
+        album = LPs().create_LP('Club Cutz Volume 3', artist=album_artist, date=1992, mixer=album_mixer)
+        album.add_track(track=track)
+
+        # Test properties creation
+        self.assertEqual('Club Cutz Volume 3', album.title)
+        self.assertEqual(album_artist, album.artist)
+        self.assertEqual(1992, album.date)
+        self.assertEqual(album_mixer, album.mixer)
+
+        # Test song searching
+        self.assertTrue(album.has_song(song_2), song_2)
+        self.assertFalse(album.has_song(song_4))
+        self.assertEqual(album.get_song_from_title('Pull Our Love Together'), song_3)
+        self.assertIsNone(album.get_song_from_title('Jump'))
+
+        # Test error conditions
+        with pytest.raises(TypeError):
+            album = LPs().create_LP('Club Cutz Volume 9', album_artist, '1992')
+        with pytest.raises(ArtistException):
+            album = LPs().create_LP('Club Cutz Volume 9', 'DC Magnet', 1992)
+        with pytest.raises(TrackListException):
+            album.add_track('Hello')
+        with pytest.raises(ArtistException):
+            album = LPs().create_LP('Club Cutz Volume 9', artist=album_artist, date=1992, mixer='Hello')
+
+        # Test string output
+        expected_string = 'Club Cutz Volume 3\n'
+        expected_string += 'Various Artists\n'
+        expected_string += 'Mixed by DJ Funk\n'
+        expected_string += '1992'
+        expected_string += 'Side 1\n'
+        expected_string += ' 1. Jump To The Beat\nDannii Minogue\n(12" Mix)\n'
+        expected_string += ' 2. Jump!\nThe Movement\n(Everybody Mix)\n'
+        expected_string += ' 3. Pull Our Love Together\nPandella\n(Komix Club Mix)\n'
+        self.assertEqual(expected_string, str(album))
+
+    def test_lps(self):
+
+        self.maxDiff = 2000
+
+        # Clean any debris in the album list first
+        all_lps = LPs()
+        for lp in all_lps.lps:
+            all_lps.delete_lp(lp)
+
+        self.assertIsNone(all_lps.find_lp_by_title('Club Cutz Volume 3'))
+        self.assertEqual(0, len(all_lps.lps))
+
+        song_1 = Song('Jump To The Beat', Artists().create_Artist('Dannii Minogue'), mix='12" Mix')
+        song_2 = Song('Jump!', Artists().create_Artist('The Movement'), mix='Everybody Mix')
+        song_3 = Song('Pull Our Love Together', Artists().create_Artist('Pandella'), mix='Komix Club Mix')
+        track_1 = TrackList()
+        track_1.add_song(song_1)
+        track_1.add_song(song_2)
+        track_1.add_song(song_3)
+
+        album_1_artist = Artists().create_Artist('Various Artists')
+        album_1 = LPs().create_LP('Club Cutz Volume 3', artist=album_1_artist, date=1992)
+        album_1.add_track(track=track_1)
+
+        song_4 = Song('He\'s All I Want', Artists().create_Artist('Angelmoon'), mix='Cappery Mix')
+        song_5 = Song('Push Upstaires', Artists().create_Artist('Underworld'), mix='Roger\'s Blue Plastic People Mix')
+        song_6 = Song('Freakin\' You', Artists().create_Artist('Jungle Brothers'), mix='Caribbean Sunshine Remix By The Buffalo Bunch')
+        track_2 = TrackList()
+        track_2.add_song(song_4)
+        track_2.add_song(song_5)
+        track_2.add_song(song_6)
+        album_2_artist = Artists().create_Artist('Various Artists')
+        album_2 = LPs().create_LP('Various: 01 Dance Music: Modernlife', artist=album_2_artist, date=2000)
+        album_2.add_track(track_2)
+
+        # Test Singleton
+        all_lps_2 = LPs()
+        self.assertEqual(all_lps, all_lps_2)
+
+        # Test existence
+        self.assertTrue(all_lps.lp_exists(album_2))
+        missing_album = LPs().create_LP('Missing Gold', artist=Artists().create_Artist('The Gold Diggers', skip_adding_to_artists_list=True), date=1920, skip_adding_to_lp_list=True)
+        self.assertFalse(all_lps.lp_exists(missing_album))
+
+        # Test we can only add valid albums
+        with pytest.raises(LPException):
+            all_lps.add_lp('Hello')
+
+        # Test adding an album
+        all_lps.add_lp(missing_album)
+        self.assertTrue(all_lps.lp_exists(missing_album))
+
+        # Test deleting an album
+        all_lps.delete_lp(missing_album)
+        self.assertFalse(all_lps.lp_exists(missing_album))
+
+        # Test finding an album by title
+        self.assertIsNone(all_lps.find_lp_by_title('Missing Gold'))
+        self.assertEqual(album_1, all_lps.find_lp_by_title('Club Cutz Volume 3'))
+
+        # Test string output
+        expected_string = 'Club Cutz Volume 3\n'
+        expected_string += 'Various Artists\n'
+        expected_string += '1992\n'
+        expected_string += ' 1. Jump To The Beat\nDannii Minogue\n(12" Mix)\n'
+        expected_string += ' 2. Jump!\nThe Movement\n(Everybody Mix)\n'
+        expected_string += ' 3. Pull Our Love Together\nPandella\n(Komix Club Mix)\n'
+        expected_string += 'Various: 01 Dance Music: Modernlife\n'
+        expected_string += 'Various Artists\n'
+        expected_string += '2000\n'
+        expected_string += ' 1. He\'s All I Want\nAngelmoon\n(Cappery Mix)\n'
+        expected_string += ' 2. Push Upstaires\nUnderworld\n(Roger\'s Blue Plastic People Mix)\n'
+        expected_string += ' 3. Freakin\' You\nJungle Brothers\n(Caribbean Sunshine Remix By The Buffalo Bunch)\n'
+        self.assertEqual(expected_string, str(all_lps))
